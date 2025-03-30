63d62c6633a8e1914379f32af2bc71626a6c63e7
Atharv Wagh <anujwagh243@gmail.com>
2025-03-30 16:15:32 +0530

    Using nested loop

---
+++ alphabatpattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class alphabatpattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the char");
+        char ch=sc.next().charAt(0);
+        for(char i='A';i<='C';i++){
+            for(char j='A';j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
+
