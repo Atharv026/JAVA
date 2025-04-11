583e48ef63937c448941a208bfb81f9ce49ef3db
Atharv Wagh <anujwagh243@gmail.com>
2025-04-11 23:40:39 +0530

    Add files via upload

---
+++ arraynumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class arraynumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    System.out.println("enter the size");
+    int size=sc.nextInt();
+        int []arr=new int[size];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        arr[3]=40;
+        arr[4]=50;
+        
+            System.out.println (arr[0]);
+            System.out.println (arr[1]);
+            System.out.println (arr[2]);
+            System.out.println (arr[3]);
+            System.out.println (arr[4]);
+            
+        
+
+    }
+    
+}
+
+
+
+/*enter the size
+5
+10
+20
+30
+40
+50 */
